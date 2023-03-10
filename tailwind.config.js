6252e38b7e27a941e0b87fa8b5b8169949a5a241
Finhub SS <fhss_ac@iMac-Finhub.local>
2023-03-10 16:50:41 +0200

    FINALLY I GOT IT

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      boxShadow: {
+        '1pl': '10px 5px 5px #e6e4e6'
+      }
+    },
+  },
+  plugins: [],
+}
